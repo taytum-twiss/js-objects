5a51a4daea9c5fa5eb24195bf1f1ef36ce387888
twisstr <twisstr@g.crookcountyschools.org>
2021-03-15 16:14:58 -0700

    lab 54 completed

--- object.js
+++ object.js
@@ -1,3 +1,4 @@
+//day 1
 let firstName = 'Taytum'
 let lastName = 'Twiss'
 let age = '15'
@@ -11,3 +12,7 @@ console.log(age)
 console.log(school)
 console.log(state)
 console.log(favoriteFood)
+
+//day 2
+let favoriteSubjects =[{name:'math'}, {name:'english'}, {name:'chemistry'}];
+let family = [{name:'Amber/Mom'}, {name:'Ross/Dad'}];
