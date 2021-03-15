6c8b81bb3d09448581ca59895b07dc86c3bec831
twisstr <twisstr@g.crookcountyschools.org>
2021-03-15 16:07:17 -0700

    lab 53 completed

---
+++ object.js
@@ -0,0 +1,13 @@
+let firstName = 'Taytum'
+let lastName = 'Twiss'
+let age = '15'
+let school = 'Crook County High School'
+let state = 'Oregon'
+let favoriteFood = 'Italian'
+
+console.log(firstName)
+console.log(lastName)
+console.log(age)
+console.log(school)
+console.log(state)
+console.log(favoriteFood)
